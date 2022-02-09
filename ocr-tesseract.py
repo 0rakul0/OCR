763f8ea72c494805722594300d4ec5fc156cd3dd
Jefferson dos Anjos <0rakul0render@gmail.com>
2022-02-09 18:46:09 -0300

    att intalação do tesseract

---
+++ ocr-tesseract.py
@@ -0,0 +1,24 @@
+import pytesseract
+import cv2
+
+"""para baixar para windows"""
+"""https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-v5.0.1.20220118.exe"""
+"""suport de linguagens https://github.com/tesseract-ocr/tessdata"""
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+#caminho da imagem
+imagem = cv2.imread("img/imgem.png")
+imagem_dois = cv2.imread("img/sem_sofrencia.png")
+#mostrando a imagem
+cv2.imshow("imagem",imagem_dois) #lembrando que o canal está no BGR
+
+#comando para conveter o canal para de BGR para RGB
+imagem = cv2.cvtColor(imagem_dois, cv2.COLOR_BGR2RGB)
+cv2.waitKey(0)
+
+config_tesseract = '--tessdata-dir tessdata'
+#conversão imagem >> texto
+texto = pytesseract.image_to_string(imagem, lang="por", config=config_tesseract)
+
+#saida de informação
+print(texto)
